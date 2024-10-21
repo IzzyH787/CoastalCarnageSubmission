a9421748babeb526d58950bea5f478053300e244
IzzyH787 <isabel.hampton@mail.bcu.ac.uk>
2024-10-21 19:48:04 +0100

    Collisions and enemy spawning
    -Added full collisions
    -Enemy spawning
    -Collison detection between enemy and player

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene(); //create scene
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); //create scene camera
 const loader = new THREE.TextureLoader();
 //setting up renderer
-const renderer = new THREE.WebGLRenderer(); //create renderer
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true}); //create renderer
 
 renderer.setSize( window.innerWidth, window.innerHeight ); //set size of renderer to window' size
 renderer.setAnimationLoop( animate ); //creates loop so renderer draws scene every time the screen refreshes
@@ -35,31 +35,55 @@ class Box extends THREE.Mesh{
 
         this.bottomPosition = this.position.y - this.height / 2;
         this.topPosition = this.position.y + this.height / 2;
+        this.leftPosition = this.position.x - this.width / 2;
+        this.rightPosition = this.position.x + this.width / 2;
+        this.frontPosition = this.position.z + this.depth / 2;
+        this.backPosition = this.position.z - this.depth / 2;
 
         this.velocity = velocity;
         this.gravity = -0.002;
         
     }
 
-    update(ground)
-    {
-        //get top and bottom position so can check for collisions
+    updateSides(){
+        //set edge positions so can check for collisions
         this.bottomPosition = this.position.y - this.height / 2;
         this.topPosition = this.position.y + this.height / 2;
+        this.leftPosition = this.position.x - this.width / 2;
+        this.rightPosition = this.position.x + this.width / 2;
+        this.frontPosition = this.position.z + this.depth / 2;
+        this.backPosition = this.position.z - this.depth / 2;
+    }
+
+    update(ground)
+    {
+        this.updateSides();
+        
 
         //apply movement
         this.position.x += this.velocity.x;
         this.position.z += this.velocity.z;
-        this.applyGravity();
+
+        // //detect collision on x  and z axis 
+        // const xCollision = ( (this.rightPosition >= ground.leftPosition) && (this.leftPosition <= ground.rightPosition) );
+        // const yCollision = ( (this.bottomPosition <= ground.topPosition) && (this.topPosition >= ground.bottomPosition) );
+        // const zCollision = ( (this.frontPosition >= ground.backPosition) && (this.backPosition <= ground.frontPosition) );
+        
+        // if (xCollision && yCollision && zCollision){
+        //     console.log("Collision");
+        // }
+
+        this.applyGravity(ground);
         
         
     }
-    applyGravity(){
+    applyGravity(ground){
         this.velocity.y += this.gravity; //add gravity
+
         //check for collsion with floor (1 frame ahead)
-        if (this.bottomPosition + this.velocity.y <= ground.topPosition){
+        if (boxCollision({box1: this, box2: ground})){
             //hitting floor
-            this.velocity.y *= 0.8; //reduce velocity to create friciton between bounces, reduce bounce height
+            this.velocity.y *= 0.5; //reduce velocity to create friciton between bounces, reduce bounce height
             this.velocity.y = -this.velocity.y; //reverse velocity for bounce effect
         }
         else{
@@ -71,6 +95,15 @@ class Box extends THREE.Mesh{
 
 //////////////DEFINING FUNCTIONS///////////////////
 
+
+const boxCollision=({box1, box2})=>{
+    const xCollision = ( (box1.rightPosition >= box2.leftPosition) && (box1.leftPosition <= box2.rightPosition) );
+    //frame ahead for y collision, gravity
+    const yCollision = ( (box1.bottomPosition + box1.velocity.y <= box2.topPosition) && (box1.topPosition >= box2.bottomPosition) );
+    const zCollision = ( (box1.frontPosition >= box2.backPosition) && (box1.backPosition <= box2.frontPosition) );
+    
+    return (xCollision && yCollision && zCollision)
+}
 //create a tree
 const createTree=()=>{
     //create trunk
@@ -100,31 +133,52 @@ const createTree=()=>{
     scene.add(treeGroup);
 }
 //function to animate geometry
+let frames = 0;
+let spawnRate = 500;
 function animate() {
-   renderer.render(scene, camera);
+    //const animationId = requestAnimationFrame(animate);
 
-   //movement code
-   player.velocity.x = 0 //stop player at start of frame
-   player.velocity.z = 0 //stop player at start of frame
-   if (keys.a.pressed){
-    player.velocity.x = -0.01; //move left
-   }
-   else if (keys.d.pressed){
-    player.velocity.x = 0.01; //move right
-   }
-   else if (keys.w.pressed){
-    player.velocity.z = -0.01; //move right
-   }
-   else if (keys.s.pressed){
-    player.velocity.z = 0.01; //move right
-   }
-   //check position of player for collisions
-   player.update(ground);
+    renderer.render(scene, camera);
 
-   //adding gravity to player
+    //movement code
+    player.velocity.x = 0 //stop player at start of frame
+    player.velocity.z = 0 //stop player at start of frame
+
+    if (keys.a.pressed){player.velocity.x = -0.01;} //move left
+    else if (keys.d.pressed){player.velocity.x = 0.01; } //move right
+    //sepeare if for axis allows them to be true at same time
+    if (keys.w.pressed){player.velocity.z = -0.01;} //move forward
+    else if (keys.s.pressed){player.velocity.z = 0.01; } //move backward
 
 
-   //player.position.y += -0.01;
+    //check position of player for collisions
+    player.update(ground);
+    //update enemy
+    enemies.forEach((enemy) => {
+    enemy.update(ground);
+    if (boxCollision({box1: player, box2: enemy})){
+        console.log("boom");
+        //cancelAnimationFrame(animationId);
+    }
+    })
+
+    if (frames % spawnRate == 0){
+        //create an enemy
+        const newEnemy = new Box({
+            width: 1, 
+            height: 1,
+            depth: 1,
+            color: 0xffff00,
+            velocity: {x: 0, y: -0.01, z:0.005},
+            //Math.random() generates random value 0-1
+            pos: {x: Math.random() - 0.5 * 20, y: 3, z: -10}
+        });
+        newEnemy.castShadow = true;
+        scene.add(newEnemy); //add to scene
+        enemies.push(newEnemy); //add to enemy array
+    }
+
+    frames++; //increment frame number
 }
 
 
@@ -158,6 +212,9 @@ const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 directionalLight.castShadow = true;
 scene.add( directionalLight );
 
+
+scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
 //create ground
 const ground = new Box({
     width: 20,
@@ -185,6 +242,10 @@ const player = new Box({
 player.castShadow = true;
 scene.add(player);
 
+const enemies = []; //create array to store enemies
+
+
+//input stuff
 const keys = {
     a: {
         pressed: false
@@ -214,6 +275,9 @@ window.addEventListener('keydown', (event) => {
         case 'KeyD':
             keys.d.pressed = true;
             break;
+        case 'Space':
+            player.velocity.y = 0.1;
+            break;     
     }
 })
 window.addEventListener('keyup', (event) => {
@@ -226,7 +290,7 @@ window.addEventListener('keyup', (event) => {
             break;
         case 'KeyS':
             keys.s.pressed = false;
-            bwreak;
+            break;
         case 'KeyD':
             keys.d.pressed = false;
             break;
